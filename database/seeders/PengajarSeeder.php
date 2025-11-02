bb2e8675327cfc83ce3876279242cf64925f1844
ferrrr777 <tenjetdoang193.com>
2025-11-02 20:01:07 +0700

    Tambah RescheduleSeeder dan update seeder lainnya

--- database/seeders/PengajarSeeder.php
+++ database/seeders/PengajarSeeder.php
@@ -5,16 +5,16 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class PengajarSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            ['name' => 'admin','email' => 'admin@example.com', 'password' => bcrypt('12345678'),'role' => 'admin'],
-           
+        DB::table('pengajars')->insert([
+            ['nama' => 'pengajar','email' => 'pengajar1@example.com', 'password' => bcrypt('12345678'),'alat_id' => 1],
+                    
         ]);
     }
 }
