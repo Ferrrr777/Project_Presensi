0ef0e229cf4a7b508c4b4af644ec383a33fd1d15
ferrrr777 <tenjetdoang193.com>
2025-11-04 00:17:19 +0700

    Logo no reload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,8 +88,8 @@
 
     .sidebar-logo {
  background-image: url('/images/logo.png'); /* path gambar */
-  width: 120px;       /* pastikan ada width tetap */
-  height: 100px;      /* height tetap */
+  width: 220px;       /* pastikan ada width tetap */
+  height: 200px;      /* height tetap */
   background-size: contain;  /* agar proporsional */
   background-repeat: no-repeat;
   background-position: center;
